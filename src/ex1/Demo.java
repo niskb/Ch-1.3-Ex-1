8ccdac2e8a7595ff6f4486091cd1d795be28d2cc
niskb <niskb19@gmail.com>
2020-02-05 11:18:30 -0500

    Demo
    Displays whether sunny or cloudy across 10 days.

---
+++ src/ex1/Demo.java
@@ -0,0 +1,53 @@
+package ex1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// double a11 = .75;
+		// double a12 = .5;
+		// double a21 = .25;
+		// double a22 = .5;
+		boolean isSunny = true;
+		int numberOfTrials = 10;
+
+		System.out.println("Day 1: Heads... Sunny");
+		for (int i = 1; i < numberOfTrials; i++) {
+			System.out.print("Day " + (i + 1) + ": ");
+			flip(isSunny);
+		}
+
+	}
+
+	public static boolean flip(boolean isSunny) {
+		if (isSunny) { // if previous day was sunny, flip once
+			if (Math.random() < 0.5) {
+				System.out.println("Heads... Sunny");
+				return true;
+			} else {
+				System.out.println("Tails... Cloudy");
+				return false;
+			}
+		} else { // if previous day was cloudy, flip twice (this code never gets reached)
+			if (Math.random() < 0.5) {
+				System.out.println("Heads on first toss");
+				if (Math.random() < 0.5) {
+					System.out.println("Heads... Sunny");
+					return true;
+				} else {
+					System.out.println("Tails... Cloudy");
+					return false;
+				}
+			} else {
+				System.out.println("Tails on first toss");
+				if (Math.random() < 0.5) {
+					System.out.println("Heads... Sunny");
+					return true;
+				} else {
+					System.out.println("Tails... Cloudy");
+					return false;
+				}
+			}
+		}
+	}
+
+}
